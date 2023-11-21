c31172b5b72f5e858c615515852e54694f4eaba0
tadeogonzalo <tadeogonzalo@gmail.com>
2023-11-21 00:31:15 -0300

    Add files via upload

---
+++ src/ProductManager.js
@@ -0,0 +1,165 @@
+import fs from 'fs';
+import { title } from 'process';
+
+export default class ProductManager {
+
+    constructor( path ){
+
+        this.path = path;
+    }
+
+    async getProducts(){
+
+        try{
+
+            if( fs.existsSync(this.path) )
+            {
+                const productos = JSON.parse(await fs.promises.readFile( this.path, 'utf-8'))
+                
+                return productos;
+
+            }else{
+
+                return [];
+            }
+
+        }catch (err){
+
+            return err;
+        }
+    }
+
+    async addProduct( producto ){
+
+        if( !producto.title || !producto.description || !producto.price || !producto.thumbnail || !producto.code || !producto.stock )
+        {
+            return "All fields are required";
+        }
+
+        const productos = await this.getProducts();
+
+        let existe = productos.find(p => p.code == producto.code)
+
+        if(existe){
+
+            return"The Product Code Already Exist.";
+
+        }else{
+            
+            if( productos.length === 0)
+            {
+                producto.id = 1;
+            }else{
+
+                producto.id = productos[ productos.length-1 ].id + 1;
+            }
+
+            productos.push( producto );
+
+            await fs.promises.writeFile( this.path, JSON.stringify(productos, null, '\t') )
+            
+            return productos;
+        }
+
+        
+    }
+
+    async getProductById(productId){
+
+        const productos = await this.getProducts();
+
+        let producto = productos.find(producto => producto.id == productId)
+
+        if(producto){
+            return producto;
+        }else{
+            return "Product Not Found"
+        }
+    }
+
+    async updateProduct(id, updateProduct ){
+
+        try{
+
+            const productos = await this.getProducts();
+
+            const productoIndex = productos.findIndex(prod => prod.id === id)
+
+            if ( productoIndex != -1 ) {
+
+                if( updateProduct.title )
+                {
+                    productos[ productoIndex ].title = updateProduct.title;
+                }
+
+                if( updateProduct.description )
+                {
+                    productos[ productoIndex ].description = updateProduct.description;
+                }
+
+                if( updateProduct.price )
+                {
+                    productos[ productoIndex ].price = updateProduct.price;
+                }
+
+                if( updateProduct.thumbnail )
+                {
+                    productos[ productoIndex ].thumbnail = updateProduct.thumbnail;
+                }
+
+                if( updateProduct.code )
+                {
+                    productos[ productoIndex ].code = updateProduct.code;
+                }
+
+                if( updateProduct.stock )
+                {
+                    productos[ productoIndex ].stock = updateProduct.stock;
+                }
+                
+                await fs.promises.writeFile( this.path, JSON.stringify(productos, null, '\t') );
+
+                return "Product Updated Successfully."
+
+            } else {
+
+                return "Prodcut not found";
+            }
+
+        }catch (err){
+
+            return err;
+
+        }
+    }
+
+    async deleteProduct( productId ){
+
+        try{
+
+            const productos = await this.getProducts();
+
+            const productoIndex = productos.findIndex( p => p.id == productId );
+
+            if( productoIndex != -1 )
+            {
+                productos.splice( productoIndex, 1);
+
+                await fs.promises.writeFile( this.path, JSON.stringify( productos, null, '\t') );
+
+                return "Product Delete Successfully.";
+            }else{
+
+                return "Prodcut not found";
+            }
+
+
+        }catch (err){
+
+            return err;
+
+        }
+
+    }
+
+}
---
+++ src/app.js
@@ -0,0 +1,37 @@
+import express from "express";
+import ProductManager from './ProductManager.js';
+
+const productManager = new ProductManager('./files/productos.json');
+
+const PORT = 8081;
+
+const app = express();
+
+app.use( express.urlencoded( { extended:true } ) );
+
+app.get( '/products', async ( req, res ) => {
+
+    let limit = req.query.limit;
+
+    let productos = await productManager.getProducts();
+
+    if( !limit ) return res.send(productos);
+
+    const productosFiltrados = productos.filter( producto => producto.id <= limit );
+
+    res.send( productosFiltrados );
+    
+});
+
+app.get( '/products/:idProduct', async ( req, res ) => {
+
+    const idProduct = req.params.idProduct;
+
+    let produto = await productManager.getProductById( idProduct );
+
+    res.send(produto);
+});
+
+app.listen( PORT, () => {
+    console.log(`Server Corriendo en puerto: ${PORT}`);
+});
